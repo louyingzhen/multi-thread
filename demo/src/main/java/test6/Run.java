72b427dbf26d9a28ec4b4bba23dd5d6f4dac6fb3
lyz <15067659390@163.com>
2019-09-21 17:12:47 +0800

    多线程练习（2019-09-21下午）

---
+++ demo/src/main/java/test6/Run.java
@@ -0,0 +1,10 @@
+package test6;
+
+public class Run {
+    public static void main(String[] args) {
+        CountOperate c = new CountOperate();
+        Thread t1 = new Thread(c);
+        t1.setName("A");
+        t1.start();
+    }
+}
