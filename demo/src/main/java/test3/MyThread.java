0d34a65edaf9420c13a76f0898274b910f0be6a2
lyz <15067659390@163.com>
2019-09-21 15:44:53 +0800

    多线程练习代码

---
+++ demo/src/main/java/test3/MyThread.java
@@ -0,0 +1,14 @@
+package test3;
+
+public class MyThread extends Thread {
+    private int count = 5;
+
+    @Override
+    synchronized public void run() {
+        super.run();
+        count--;
+        //线程共享数据的示例不要使用for语句，因为使用同步后其他线程就得不到运行机会了
+        //一直由一个线程进行减法运算
+        System.out.println("由" + this.currentThread().getName() + "计算，count=" + count);
+    }
+}
