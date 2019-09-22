77599b43f4c6ed051842447ada512130d02643b2
lyz <15067659390@163.com>
2019-09-22 16:40:04 +0800

    多线程代码练习（2019-09-22）

---
+++ demo/src/main/java/test10/MyThread.java
@@ -0,0 +1,11 @@
+package test10;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < 5000; i++) {
+            System.out.println("i=" + (i + 1));
+        }
+    }
+}
