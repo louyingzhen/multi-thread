582188c015482e19096052ad68429d63b47e2239
lyz <15067659390@163.com>
2019-09-28 16:20:28 +0800

    2019-09-28多线程练习

---
+++ demo/src/main/java/stopThrowLock/MyThread.java
@@ -0,0 +1,15 @@
+package stopThrowLock;
+
+public class MyThread extends Thread {
+    private SynchronizedObject object;
+
+    public MyThread(SynchronizedObject object) {
+        super();
+        this.object = object;
+    }
+
+    @Override
+    public void run() {
+        object.printString("b", "bb");
+    }
+}
